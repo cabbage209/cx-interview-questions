5d2f412690eeb34bde91e1aa16f7991276c9eab3
Oliver O'Brien-Elliot <obrienelliot@yahoo.co.uk>
2020-09-24 18:58:02 +0100

    added sub-total calculation to Basket class + created pricer class

--- shopping_basket/shopping_basket.py
+++ shopping_basket/shopping_basket.py
@@ -9,15 +9,23 @@ class Basket:
         for i in args:
             quan = i.quantity
             n = i.name
+            p = i.price
             if type(n) == str:
                 for x in range(quan):
                     self.items.append(n)
+                    self.sub_total += p
+                    print(self.sub_total)
             else:
                 raise TypeError
-
+        
 
 class Item:
     def __init__(self, name, price, quantity):
         self.name = name
         self.price = price
         self.quantity = quantity
+
+
+class Pricer:
+    def __init__(self, obj):
+        return
--- shopping_basket/shopping_basket_tests/test_shopping_basket.py
+++ shopping_basket/shopping_basket_tests/test_shopping_basket.py
@@ -1,6 +1,7 @@
 import pytest
 from shopping_basket import Basket
 from shopping_basket import Item
+from shopping_basket import Pricer
 
 
 def test_default_basket():
@@ -28,9 +29,23 @@ def test_add_item_success(empty_basket, gen_item):
     assert len(empty_basket.items) == 2
 
 def test_add_numerous_items(empty_basket):
-    item = Item('fish', 2.0, 2)
-    item1 = Item('crab', 1.0, 4)
-    item2 = Item('shark', 5.0, 1)
+    item = Item('fish', 2.10, 2)
+    item1 = Item('crab', 1.02, 4)
+    item2 = Item('shark', 4.31, 1)
     empty_basket.add_item(item, item1, item2)
     assert len(empty_basket.items) == 7
+    assert empty_basket.sub_total == 12.59
+
+@pytest.fixture
+def full_basket(empty_basket):
+    item = Item('fish', 2.10, 2)
+    item1 = Item('crab', 1.02, 4)
+    item2 = Item('shark', 4.31, 1)
+    empty_basket.add_item(item, item1, item2)
+    return empty_basket
+
+    
+def test_default_pricer(full_basket):
+    pricer = Pricer(full_basket)
+    #assert full_basket.sub_total == 13.0
     
