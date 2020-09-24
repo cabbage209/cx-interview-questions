fab85a9971bc6a35998f8280f752c481c58e190c
Oliver O'Brien-Elliot <obrienelliot@yahoo.co.uk>
2020-09-24 20:38:21 +0100

    completed bonus question + ensured totals < 0

--- shopping_basket/shopping_basket.py
+++ shopping_basket/shopping_basket.py
@@ -14,7 +14,8 @@ class Basket:
                 for x in range(quan):
                     self.items.append(n)
                     self.sub_total += p
-                    print(self.sub_total)
+                    if self.sub_total < 0.0:
+                        self.sub_total = 0.0
             else:
                 raise TypeError
 
@@ -32,11 +33,43 @@ class Pricer:
         try:
             # crab_num divis by 4 without remiander
             y = crab_num // 4
-            obj.discount = 1.02 * y
+            obj.discount += 1.02 * y
         except y == 0:
-            print("x not divisable by 4")
-        obj.total = obj.sub_total - obj.discount
+            print("crab_num not divisable by 4")
+        try:
+            obj.total = obj.sub_total - obj.discount
+        except obj.total < 0.0:
+            obj.total = 0.0
         print(
             f"""(Baskets subtotal = {obj.sub_total}, Baskets discount = {obj.discount},
-            Baskets total = {obj.total} )"""
+            Baskets total = {round(obj.total, 2)} )"""
+        )
+
+
+class Bonus:
+    def __init__(self, obj):
+        fish_num = obj.items.count("fish")
+        shark_num = obj.items.count("shark")
+        try:
+            # fish_num divis by 3 without remiander
+            y = fish_num // 3
+            obj.discount += 2.1 * y
+            print(obj.discount)
+        except y == 0:
+            print("fish_num not divisable by 3")
+        try:
+            # shark_num divis by 2 without remiander
+            y = shark_num // 3
+            obj.discount += 5.0 * y
+            print(obj.discount)
+        except y == 0:
+            print("shark_num not divisable by 3")
+        try:
+            obj.total = obj.sub_total - obj.discount
+        except obj.total < 0.0:
+            obj.total = 0.0
+        print(
+            f"""(Baskets subtotal = {round(obj.sub_total, 2)},
+            Baskets discount = {round(obj.discount, 2)},
+            Baskets total = {round(obj.total, 2)} )"""
         )
--- shopping_basket/shopping_basket_tests/test_shopping_basket.py
+++ shopping_basket/shopping_basket_tests/test_shopping_basket.py
@@ -2,6 +2,7 @@ import pytest
 from shopping_basket import Basket
 from shopping_basket import Item
 from shopping_basket import Pricer
+from shopping_basket import Bonus
 
 """define the basket class which will hold the muteable
 list of items and their subtotal"""
@@ -48,6 +49,13 @@ def test_add_numerous_items(empty_basket):
     assert empty_basket.sub_total == 12.59
 
 
+# test that cannot be subtotal < 0.0
+def test_basket_less_than_zero(empty_basket):
+    item = Item("fish", -2.19, 2)
+    empty_basket.add_item(item)
+    assert empty_basket.sub_total == 0.0
+
+
 @pytest.fixture
 def full_basket(empty_basket):
     item = Item("fish", 2.10, 2)
@@ -63,3 +71,19 @@ def test_default_pricer(full_basket):
     assert full_basket.sub_total == 12.59
     # create offer buy 3 get 1 free on crab item
     assert full_basket.total == 11.57
+
+
+@pytest.fixture
+def bonus_basket(empty_basket):
+    item = Item("fish", 2.1, 10)
+    item1 = Item("crab", 1.00, 3)
+    item2 = Item("shark", 5.00, 6)
+    empty_basket.add_item(item, item1, item2)
+    return empty_basket
+
+
+def test_default_bonus(bonus_basket):
+    Bonus(bonus_basket)
+    assert bonus_basket.sub_total == 54.0
+    # create bonus offer buy 2 get 1 free on fish and shark items
+    assert bonus_basket.total == 37.7
