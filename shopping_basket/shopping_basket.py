058bb22e5f9d325497dc4c85fb6c2040b96e8a23
Oliver O'Brien-Elliot <obrienelliot@yahoo.co.uk>
2020-09-24 18:13:45 +0100

    added the Item class and handled adding *obj's to the basket

--- shopping_basket/shopping_basket.py
+++ shopping_basket/shopping_basket.py
@@ -5,9 +5,19 @@ class Basket:
         self.total = 0.0
         self.items = []
 
-    def add_item(self, name: str, price, quantity):
-        if type(name) == str:
-            self.items.append(name)
-        else:
-            raise TypeError
+    def add_item(self, *args):
+        for i in args:
+            quan = i.quantity
+            n = i.name
+            if type(n) == str:
+                for x in range(quan):
+                    self.items.append(n)
+            else:
+                raise TypeError
 
+
+class Item:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
--- shopping_basket/shopping_basket_tests/test_shopping_basket.py
+++ shopping_basket/shopping_basket_tests/test_shopping_basket.py
@@ -1,5 +1,6 @@
 import pytest
 from shopping_basket import Basket
+from shopping_basket import Item
 
 
 def test_default_basket():
@@ -13,12 +14,23 @@ def test_default_basket():
 def empty_basket():
     return Basket()
 
-def test_add_item_success(empty_basket):
-    empty_basket.add_item('fish', 2.0, 2)
-    assert empty_basket.items == ['fish']
+def test_default_item():
+    item = Item('fish', 2.0, 2)
+    assert item.price == 2.0
 
-def test_add_item_fail(empty_basket):
-    with pytest.raises(TypeError):
-        empty_basket.add_item(3, 2.0, 2)
-        
+@pytest.fixture
+def gen_item():
+    return Item('fish', 2.0, 2)
 
+    
+def test_add_item_success(empty_basket, gen_item):
+    empty_basket.add_item(gen_item)
+    assert len(empty_basket.items) == 2
+
+def test_add_numerous_items(empty_basket):
+    item = Item('fish', 2.0, 2)
+    item1 = Item('crab', 1.0, 4)
+    item2 = Item('shark', 5.0, 1)
+    empty_basket.add_item(item, item1, item2)
+    assert len(empty_basket.items) == 7
+    
